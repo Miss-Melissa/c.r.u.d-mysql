204d361a3208440f41ba23cbbea1da5e2d2d124a
Miss-Melissa <melissahh@live.se>
2023-07-17 17:34:37 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AddProductPage from './pages/AddProductPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>C.R.U.D</h1>
+      <AddProductPage />
+    </div>
+  );
+}
+
+export default App;
