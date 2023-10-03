f701ae17719899fb89eb9e7e6fc7fc237ac9db7b
Miss-Melissa <melissahh@live.se>
2023-10-03 13:41:29 +0200

    git push origin main

--- client/src/components/product/product.js
+++ client/src/components/product/product.js
@@ -6,6 +6,7 @@ function Product({ product }) {
         <div>
             <div>
                 <h2>{product.productName}</h2>
+                <img src={`http://localhost:3001/uploads/${product.productImage}`} alt={product.productName} height={250} width={250} />
                 <p><strong>Description:</strong> {product.productDescription}</p>
                 <p><strong>Price:</strong> ${product.productPrice}</p>
             </div>
